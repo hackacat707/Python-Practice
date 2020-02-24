36321e0bb562bae3876f3982005cd7b4e57bfaa4
Ioana <ioanaavram46@yahoo.com>
2020-02-24 18:10:05 +0000

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,17 @@
+name = "Ann"
+age = 24
+height = 63
+weight = 105
+eyes = "Brown"
+teeth = "white"
+hair = "Brown"
+
+print(f"Lets talk about {name}")
+print(f"They're {height} inches tall")
+print(f"They're {weight} pounds heavy")
+print("Actually thats not that heavy")
+print(f"They're got {eyes} eyes and {hair} hair")
+print(f"Their teeth are usually {teeth} depending on the coffee")
+
+total = age + height + weight
+print(f"If I was to add {age}, {height} and {weight} I get {total}")
